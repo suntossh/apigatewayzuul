9bff04d75869a442a0fca5cfb7fa60502376e716
suntossh.numerouno@gmail.com <suntossh.numerouno@gmail.com>
2019-11-30 14:49:58 -0500

    Zull API Gateway as single point of entry and access all microservice via eureka server

---
+++ src/test/java/com/suntossh/springboot/apigatewayzuul/ApigatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suntossh.springboot.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
